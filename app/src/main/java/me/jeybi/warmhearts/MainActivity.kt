08d4fc50ef28e71c67ce875909e520ebf8665d57
jaloliddinboltaev <codewithjeyallday@gmail.com>
2020-12-04 10:04:40 +0500

    Initial commit

---
+++ app/src/main/java/me/jeybi/warmhearts/MainActivity.kt
@@ -0,0 +1,26 @@
+package me.jeybi.warmhearts
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Process
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
+
---
+++ app/src/main/java/me/jeybi/warmhearts/SettingUpActivity.kt
@@ -0,0 +1,11 @@
+package me.jeybi.warmhearts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_up)
+    }
+}
